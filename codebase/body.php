3f0547ff7ec7ea40cf9cd1e15c2b8e09d58810bb
William Thompson <williemaxt@gmail.com>
2018-03-17 12:16:29 -0400

    Update body.php
    adds an extra field for date and time

--- codebase/body.php
+++ codebase/body.php
@@ -9,7 +9,9 @@ echo '<div class="postForm">
 <form method="post" action="index.php">
 <input id="title" name="title" type="text" placeholder="What Is This?">
 <textarea name="body" placeholder="Code Goes Here..."></textarea>
-<input type="text" name="commit_time" placeholder="02/10/2018: 1:05 PM">
-<input type="submit" value="submit">
+<input type="text" name="commit_date" placeholder="02/10/2018: 1:05 PM">
+<br/>
+<br/>
+<input type="submit" name="submit" value="submit">
 </form>
 </div>';
