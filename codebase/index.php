ba22903ac8a02b7e09aa5fa1d6dc5e8458b3f6b6
William Thompson <williemaxt@gmail.com>
2018-03-17 12:13:26 -0400

    Write Functionality
    This page can write to the database by pulling info for form fields. However the data field must have this format "9999-12-31 23:59:59" typed in manually. this issue will be fixed soon

--- codebase/index.php
+++ codebase/index.php
@@ -1,5 +1,15 @@
 <?php 
     include_once 'connection.php';
+    //inserting the form inputs as variables
+    $title = $_POST['title'];
+    $body = $_POST['body'];
+    $commit_date = $_POST['commit_date'];
+
+    $sql = "INSERT INTO code (title, body, commit_date) VALUES ('$title', '$body', '$commit_date');";
+
+    mysqli_query($conn, $sql);
+    //take us back home after submition
+   // header("Location: allcode.php?commit=success");
 ?>
 <html>
 <head>
@@ -8,7 +18,9 @@
 </head>
 <body>
 <h1>611 Code Base</h1>
-<?php include ("navButtons.php");?>
+    <a id="allcode" href="allcode.php">
+        <button>+</button>
+    </a>
 <?php include ("body.php");?>
 
 </body>
