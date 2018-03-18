a09896b9cb40982a485ea1f00748bf0516893934
cmerchant1598 <cmerchant1598@gmail.com>
2018-03-18 19:38:21 -0400

    Update body.php
    Displays current Date and Time on body.php

--- codebase/body.php
+++ codebase/body.php
@@ -5,13 +5,17 @@
  * Date: 3/13/18
  * Time: 4:53 PM
  */
-echo '<div class="postForm">
+
+
+?>
+
+<div class="postForm">
 <form method="post" action="index.php">
 <input id="title" name="title" type="text" placeholder="What Is This?">
 <textarea name="body" placeholder="Code Goes Here..."></textarea>
-<input type="text" name="commit_date" placeholder="02/10/2018: 1:05 PM">
-<br/>
+<?php  echo date("Y-m-d h:i:s"); ?> 
+    <br/>
 <br/>
 <input type="submit" name="submit" value="submit">
 </form>
-</div>';
+</div>
