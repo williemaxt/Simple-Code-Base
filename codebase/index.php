485058f8c236ba9f88c47e90c0c4c40065baa733
cmerchant1598 <cmerchant1598@gmail.com>
2018-03-18 19:57:35 -0400

    Update index.php
    Added current date and time input

--- codebase/index.php
+++ codebase/index.php
@@ -4,13 +4,18 @@
     $title = $_POST['title'];
     $body = $_POST['body'];
     $commit_date = $_POST['commit_date'];
+    #this line above (line 6) is saying post the commit date variable into sql table
+    #this line below (line 8) is telling the commit date variable what the format is for both the date and time.
+    $commit_date = date("Y-m-d h:i:s");
+
 
     $sql = "INSERT INTO code (title, body, commit_date) VALUES ('$title', '$body', '$commit_date');";
-
     mysqli_query($conn, $sql);
-    //take us back home after submition
+    //take us back home after submission
    // header("Location: allcode.php?commit=success");
+
 ?>
+
 <html>
 <head>
     <title>Code Base</title>
